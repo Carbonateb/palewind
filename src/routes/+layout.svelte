ec01315696c51f41b6bcb0916ce8687d6ff48057
Lucas Champagne <31268463+Carbonateb@users.noreply.github.com>
2023-06-28 12:36:38 +1000

    Moved meta tags to app.html
    They need to be here due to disabling SSR

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,4 @@
 	import "../app.postcss";
 </script>
 
-<svelte:head>
-	<title>Palewind</title>
-	<meta
-		name="description"
-		content="An intuitive palette editor + generator specifically designed for Tailwind users, based on palettte.app"
-	/>
-	<meta
-		name="description"
-		content="An intuitive palette editor + generator specifically designed for Tailwind users."
-	/>
-	<meta property="og:title" content="Palewind Palette Editor" />
-	<meta property="og:description" content="Finally a palette editor that satisfies @Carbonateb" />
-	<meta property="og:type" content="website" />
-</svelte:head>
-
 <slot />
