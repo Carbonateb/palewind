13b12e887294c46ac0745eddcf928e797d277c74
Maldonacho <31268463+Carbonateb@users.noreply.github.com>
2024-09-11 21:44:45 +1000

    Added copy buttons

--- src/lib/palewind/color-editor/PalewindColorEditor.svelte
+++ src/lib/palewind/color-editor/PalewindColorEditor.svelte
@@ -64,10 +64,26 @@
 
 	// Read only
 	$: luminance = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+
+	function copyHex() {
+		navigator.clipboard.writeText(shade.color.replace("#", ""));
+	}
+	function copyRgb() {
+		navigator.clipboard.writeText(`${cleanNum(rgb[0])} ${cleanNum(rgb[1])} ${cleanNum(rgb[2])}`);
+	}
+	function copyHsl() {
+		const hsl = ColorConvert.RGBtoHSL(rgb);
+		navigator.clipboard.writeText(
+			`${Math.floor(hsl[0] * 360)} ${Math.round(hsl[1]) * 100}% ${Math.round(hsl[2] * 100)}%`
+		);
+	}
+	function cleanNum(num: number) {
+		return Math.round(num * 255);
+	}
 </script>
 
 <div
-	class="color-shade highlight relative flex w-0 min-w-fit flex-grow flex-col items-center gap-1 overflow-clip"
+	class="color-shade highlight group relative flex w-0 min-w-fit flex-grow flex-col items-center gap-1 overflow-clip"
 	style="background-color: {shade.color};"
 	class:light-color={luminance >= 0.5}
 	on:mousedown={onEditorClicked}
@@ -79,6 +95,30 @@
 		class="mx-2 w-10 border-b border-transparent bg-transparent text-center font-bold text-white transition-colors focus:border-white focus:outline-none"
 		tabindex="0"
 	/>
+	<div
+		class="flex w-full items-center justify-center gap-1 text-xs opacity-0 transition-opacity group-hover:opacity-100"
+	>
+		<div title="Copy to clipboard">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="lucide lucide-clipboard-copy opacity-50"
+				><rect width="8" height="4" x="8" y="2" rx="1" ry="1" /><path
+					d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2"
+				/><path d="M16 4h2a2 2 0 0 1 2 2v4" /><path d="M21 14H11" /><path d="m15 10-4 4 4 4" /></svg
+			>
+		</div>
+		<button class="opacity-50 hover:opacity-100" on:click={copyHex}>Hex</button>
+		<button class="opacity-50 hover:opacity-100" on:click={copyRgb}>RGB</button>
+		<button class="opacity-50 hover:opacity-100" on:click={copyHsl}>HSL</button>
+	</div>
 	<div class="mx-1 mb-1 flex flex-grow justify-center gap-1">
 		<Slider
 			bind:value={color[0]}
@@ -115,6 +155,9 @@
 
 	.light-color > input {
 		@apply text-black focus:border-black;
+	}
+	.light-color {
+		@apply text-black;
 	}
 
 	.delete:focus,
